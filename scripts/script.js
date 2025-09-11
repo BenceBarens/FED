7dfe9a3616324b9b2965edf9c866d80e861a5837
Bence <51375275+BenceBarens@users.noreply.github.com>
2025-09-11 23:47:31 +0200

    Initial chair layout setup

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // JavaScript Document
 console.log("hi");
+
+function toggle(){
+    console.log('Toggle circle'); 
+    document.querySelector('.chairlist').classList.add('circle');
+
+    for (let i = 1; i <= 30; i++) {
+        const el = document.getElementById(`chair-${i}`);
+        if (el) {
+        el.classList.add(`position${i}`);
+        }
+    }
+}
